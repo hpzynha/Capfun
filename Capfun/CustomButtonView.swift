c0225064272012bf65c90bb502ce6dca750e68b1
hpzynha <larissa.nogueira.rocha@gmail.com>
2024-03-15 16:35:49 +1030

    Capfun Finalized

---
+++ Capfun/CustomButtonView.swift
@@ -0,0 +1,18 @@
+//
+//  CustomButtonView.swift
+//  Capfun
+//
+//  Created by Larissa Nogueira da Rocha on 15/3/2024.
+//
+
+import SwiftUI
+
+struct CustomButtonView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CustomButtonView()
+}
