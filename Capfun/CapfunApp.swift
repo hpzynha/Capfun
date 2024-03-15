e33159393a17998d7a6b9b7e514b030ba2fb69d1
hpzynha <larissa.nogueira.rocha@gmail.com>
2024-03-15 12:16:05 +1030

    Initial Commit

---
+++ Capfun/CapfunApp.swift
@@ -0,0 +1,17 @@
+//
+//  CapfunApp.swift
+//  Capfun
+//
+//  Created by Larissa Nogueira da Rocha on 15/3/2024.
+//
+
+import SwiftUI
+
+@main
+struct CapfunApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
